ca18c3d3f34dcbf4b5c191efe51213bcecdef217
Arjang Khademi <arjangkhdm@gmail.com>
2025-12-06 20:40:41 +0100

    Initial Commit

---
+++ Coffein/CoffeinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeinApp.swift
+//  Coffein
+//
+//  Created by Arjang Khademi on 06.12.25.
+//
+
+import SwiftUI
+
+@main
+struct CoffeinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
